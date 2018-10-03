5e3604d6080bb375005fc6ff6a4863130dca9bae
Tobias Mayer <tobim@fastmail.fm>
2018-10-03 12:43:54 +0200

    Add colmn names to output csv

--- brocon18/evaluation/latency/latency_visualize.R
+++ brocon18/evaluation/latency/latency_visualize.R
@@ -1,6 +1,8 @@
 library(ggplot2)
+w = 6
+h = 4
 data <- read.csv("latency.csv")
-ggplot(data, aes(relays, rtt/1000000, group=relays))
-  + geom_boxplot()
-  + facet_wrap(~ payload, labeller=label_both)
-ggsave("latency.png", width=6, height=4, type="cairo")
+ggplot(data, aes(relays, rtt/1000000, group=relays)) + geom_boxplot() + facet_wrap(~ payload, labeller=label_both)
+ggsave("latency_box.png", width=w, height=h, type="cairo")
+ggplot(data, aes(relays, rtt, colour=payload)) + stat_ecdf()
+ggsave("latency_ecdf.png", width=w, height=h, type="cairo")
