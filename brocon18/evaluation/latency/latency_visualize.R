09b40db9abe285a8c56fb4df00b7fe3662171eb2
Tobias Mayer <tobim@fastmail.fm>
2018-10-03 15:03:25 +0200

    Use log10 for y-axis

--- brocon18/evaluation/latency/latency_visualize.R
+++ brocon18/evaluation/latency/latency_visualize.R
@@ -2,7 +2,7 @@ library(ggplot2)
 w = 6
 h = 4
 data <- read.csv("latency.csv")
-ggplot(data, aes(relays, rtt/1000000, group=relays)) + geom_boxplot() + facet_wrap(~ payload, labeller=label_both)
+ggplot(data, aes(relays, rtt/1000000, group=relays)) + geom_boxplot() + facet_wrap(~ payload, labeller=label_both) + scale_y_continuous(trans='log10')
 ggsave("latency_box.png", width=w, height=h, type="cairo")
 ggplot(data, aes(relays, rtt, colour=payload)) + stat_ecdf()
 ggsave("latency_ecdf.png", width=w, height=h, type="cairo")
